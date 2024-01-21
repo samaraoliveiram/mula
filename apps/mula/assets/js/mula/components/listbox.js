77cf8a41ab7ff391acf74459f8684cd83862ab17
Samara Motta <samaramotta1998@gmail.com>
2024-01-21 19:20:44 +0200

    add focus on keydown

--- apps/mula/assets/js/mula/components/listbox.js
+++ apps/mula/assets/js/mula/components/listbox.js
@@ -3,34 +3,61 @@ const listbox = {
     this.multiple = this.el.hasAttribute("aria-multiselectable");
 
     this.el.addEventListener("blur", (event) => {
-      this.removeDataFocused();
+      this.removeFocusedOption();
     });
 
     this.el.addEventListener("focus", (event) => {
       // focus is set on the first returned selected option or the first option
       // not disabled
-      const nextFocusedEl =
+      const nextOption =
         this.el.querySelector("[aria-selected=true]") ||
-        this.el.querySelector("[role=option]:not([disabled])");
+        this.getFirstAvailableOption();
 
-      this.updateDataFocused(nextFocusedEl);
+      this.updateFocusedOption(nextOption);
     });
 
     this.el.addEventListener("mula:listbox:clicked", (event) => {
-      this.toggleOption(event.target);
-      this.updateDataFocused(event.target);
+      this.toggleSelectedOption(event.target);
+      this.updateFocusedOption(event.target);
+    });
+
+    this.el.addEventListener("keydown", (event) => {
+      const key = event.key;
+      const focusedOption = this.el.querySelector("[data-focused=true]");
+      let nextFocusedOption;
+
+      if (key == "Home") {
+        nextFocusedOption = this.getFirstAvailableOption();
+      } else if (key == "End") {
+        nextFocusedOption = this.getLastAvailableOption();
+      } else if (key == "ArrowDown") {
+        nextFocusedOption = focusedOption.nextElementSibling;
+      } else if (key == "ArrowUp") {
+        nextFocusedOption = focusedOption.previousElementSibling;
+      }
+
+      this.updateFocusedOption(nextFocusedOption || focusedOption);
     });
   },
 
-  removeDataFocused() {
+  getFirstAvailableOption() {
+    return this.el.querySelector("[role=option]:not([disabled])");
+  },
+
+  getLastAvailableOption() {
+    options = this.el.querySelectorAll("[role=option]:not([disabled])");
+    return options[options.length - 1];
+  },
+
+  removeFocusedOption() {
     for (let option of this.el.querySelectorAll("[data-focused=true]")) {
       option.removeAttribute("data-focused");
     }
     this.el.setAttribute("aria-activedescendant", "");
   },
 
-  updateDataFocused(el) {
-    this.removeDataFocused();
+  updateFocusedOption(el) {
+    this.removeFocusedOption();
     this.el.setAttribute("aria-activedescendant", el?.id);
     el?.setAttribute("data-focused", true);
   },
@@ -53,7 +80,7 @@ const listbox = {
     el.setAttribute("data-selected", isSelected);
   },
 
-  toggleOption(el) {
+  toggleSelectedOption(el) {
     const isSelected = el.getAttribute("aria-selected") == "true";
     this.selectOption(el, !isSelected);
   },
